e585a3f9aeea697357d42247bfa43d8ba004e10b
amandaprinzo@gmail.com <amandaprinzo@gmail.com>
2016-01-24 13:40:23 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,55 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table" 
 end
 
 def deal_card
-  # code #deal_card here
+    card = rand(1..11)
+    return card
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+    answer = gets.chomp
+    return "#{answer}"
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+      2.times do
+      deal_card
+    end
+      card_total = deal_card + deal_card
+      display_card_total(card_total)
+      return card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  get_user_input
+  return card_total
+  if get_user_input == "h"
+      deal_card
+      new_total = card_total += deal_card
+      return new_total
+  elsif get_user_input == "s"
+    nil
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Sorry, I don't understand"# code invalid_command here
 end
 
 #####################################################
@@ -39,6 +57,6 @@ end
 #####################################################
 
 def runner
-  # code runner here
+
 end
     
