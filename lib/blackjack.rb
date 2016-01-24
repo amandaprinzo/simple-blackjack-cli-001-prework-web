f7ae6912de3199d94781eb7151ced60635c1616a
amandaprinzo@gmail.com <amandaprinzo@gmail.com>
2016-01-24 14:57:15 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,7 +4,6 @@ end
 
 def deal_card
     card = rand(1..11)
-    puts card
 end
 
 def display_card_total(card_total)
@@ -17,7 +16,7 @@ end
 
 def get_user_input
     answer = gets.chomp
-    puts "#{answer}"
+    #puts "#{answer}"
 end
 
 def end_game(card_total)
@@ -26,26 +25,21 @@ end
 
 def initial_round
       2.times do
-      deal_card
+      deal_card.to_i
     end
-      card_total = deal_card + deal_card
-      display_card_total(card_total)
-      puts card_total
+      sum = deal_card + deal_card
+      display_card_total(sum)
+      return sum
 end
 
 def hit?(card_total)
-  prompt_user
-  get_user_input
-  puts card_total
-  if get_user_input == "h"
-      deal_card
-      new_total = card_total += deal_card
-      puts new_total
-  elsif get_user_input == "s"
-    nil
-  else
-    invalid_command
-  end
+    prompt_user
+    get_user_input
+    return card_total
+      if get_user_input == "h"
+        deal_card
+        new_total = card_total.to_i += deal_card.to_i
+      end
 end
 
 def invalid_command
